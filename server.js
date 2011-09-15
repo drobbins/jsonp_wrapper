3eaf8aa3dc58b1b1656fee321f16a9bf057b7b4e
David Robbins <robbins.david@gmail.com>
2011-09-15 15:00:05 -0500

    Initial draft of jsonp wrapping service basically functional

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-var http = require('http');
+var request = require('request'),
+    http = require('http'),
+    querystring = require('querystring');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  query_as_string = req.url.split("?")[1];
+  query = querystring.parse(query_as_string);
+  if (!query.url || !query.callback){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('No URL or Callback function specified.\n');
+  }
+  else{
+    request(
+      {
+        url: query.url,
+        headers: {
+          Accept: query.type ||  "application/json,text/plain,text/html,*/*"
+        }
+      },
+      function(error, response, body){
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        if(body.match(/^\{.*\}$/)){ // Don't wrap data that is already JSON
+          res.end("(function(){"+query.callback+"({contents:"+body+"});)");
+        }
+        else{
+          res.end("(function(){"+query.callback+"({contents:'"+body+"'});)");
+        }
+    });
+  }
 }).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
